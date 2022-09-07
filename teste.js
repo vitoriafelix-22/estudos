04ab73fc70bd16ceffa402d78d7d491c4a69e0c3
Vitória Félix <110747719+vitoriafelix-22@users.noreply.github.com>
2022-09-07 15:04:08 -0300

    Add files via upload

---
+++ teste.js
@@ -0,0 +1,8 @@
+function getValue(){
+    var value = document.getElementById("name").value
+    document.getElementById("result").innerHTML = "Bem vindo(a),  " + value + "!"
+}
+function removeValue(){
+    document.getElementById("name").value = " "
+    document.getElementById("result").innerHTML = ""
+}
